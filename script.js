d80fe0321941147f0ac92e75f40bec912a752263
Jessica Helft <jessicahelft@gmail.com>
2020-05-29 22:00:25 -0500

    first draft of day planner html and pseudo code for script

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+//open planner -->
+
+//display currentDay using momemt.js-->
+
+//Timeblocks should show stacked on top of eachother and should have corresponding times. Each block should be an hour from 9AM-5PM. -->
+
+//Timeblock should have a button next to it to save stored event text. (local storage) -->
+
+//When looking at timeblocks for that day each timeblock is color coded to indicate whether it is in the past (grey), present (red), or future (green). -->
+
+//Timeblocks can be clicked on. When you click you should be able to enter text for an event you might plan. -->
+
+//when page is refreshed the events should be in the planner still.
+
+//https://vanderbilt.bootcampcontent.com/vanderbilt_coding_bootcamp/vu-nsh-fsf-ft-05-2020-u-c/-/raw/master/05-Third-Party-APIs/02-HomeworkAssets/05-third-party-apis-homework-demo.gif-->
