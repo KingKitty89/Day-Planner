09c83afd3a6d22b91de1679504306c384dd23b0f
Jessica Helft <jessicahelft@gmail.com>
2020-05-30 20:07:14 -0500

    added local storage, and hour tracking functionality

--- script.js
+++ script.js
@@ -1,5 +1,71 @@
 
 
+$(document).ready(function () {
+
+    var today = moment().format('dddd, MMMM Do, YYYY');
+    console.log(today);
+    $("#currentDay").text(today);
+
+    $(".saveBtn").on("click", function () {
+        var time = $(this).parent().attr("id");
+        var description = $(this).siblings(".description").val();
+        console.log(description, time)
+
+        localStorage.setItem(time, description);
+        
+   
+    })
+
+        var currentHour = moment().hour();
+        console.log(currentHour);
+        
+        $(".time-block").each(function () {     
+       
+        var blockHour = parseInt($(this).siblings().attr("id").split("hour")[1]);
+       
+       
+        console.log(blockHour, currentHour);
+
+
+        if (blockHour < currentHour){
+            $(this).addClass("past");
+            
+        }
+
+        if (blockHour === currentHour){
+            $(this).addClass("present");
+        }
+
+        else {
+            $(this).addClass("future");
+        }
+    
+       })
+  
+    
+});
+
+
+//  CHANGE INPUT BOX COLOR BASED ON TIME OF DAY
+
+
+
+
+
+
+
+
+
+
+
+//var currentTime = moment();
+//console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+//var currentHour = parseInt(moment().hours());
+//console.log(currentHour);
+
+
+
 //open planner -->
 
 //display currentDay using momemt.js-->
@@ -15,3 +81,4 @@
 //when page is refreshed the events should be in the planner still.
 
 //https://vanderbilt.bootcampcontent.com/vanderbilt_coding_bootcamp/vu-nsh-fsf-ft-05-2020-u-c/-/raw/master/05-Third-Party-APIs/02-HomeworkAssets/05-third-party-apis-homework-demo.gif-->
+
